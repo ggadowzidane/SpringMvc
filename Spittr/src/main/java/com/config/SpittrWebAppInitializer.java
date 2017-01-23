ac1179b72b8773ede17e84fac552453b9d4a41d2
ggadow <daebare@gmail.com>
2017-01-23 20:23:05 +0900

    - 2017.01.23 -
    스프링 MVC 기본설정 및 HomeControoler 생성

---
+++ Spittr/src/main/java/com/config/SpittrWebAppInitializer.java
@@ -0,0 +1,25 @@
+package com.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	//Servlet URL Mapping 설정
+	protected String[] getServletMappings() {
+		return new String[]{"/"};
+	}
+
+}
